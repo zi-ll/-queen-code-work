03712a4d738af7c72671f05458b53e45fc85e4f7
zi-ll <72028809+zi-ll@users.noreply.github.com>
2020-11-07 07:33:02 +0300

    jquery work

---
+++ whack-game/whack.js
@@ -0,0 +1,15 @@
+$("whack1").click(function(){
+    $(".mole1").hide();
+});
+    
+$("INCORRECT").click(function(){
+    $("INCORRECT").hide();
+});
+    
+$("INCORRECT").click(function(){
+    $("INCORRECT").hide();
+});
+    
+$("INCORRECT").click(function(){
+    $("INCORRECT").show();
+});
